e61b2fa098bb8eb42a0e329072a21d2f76c24b89
clifford <clifford@noukod.com>
2018-10-18 17:19:14 +0000

    automatically backed up by learn

--- bin/run.rb
+++ bin/run.rb
@@ -1,12 +1,11 @@
 # require_relative '../config/environment'
-#
+# 
 # Hashie
 # Sinatra
 # Octokit
 # Pry
 # AwesomePrint
-require 'bundler/setup'
-Bundler.require(:default, :development)
+require_relative '../config/environment'
 
  Hashie
  Sinatra
